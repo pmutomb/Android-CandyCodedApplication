fad3a9029edff935558d833477f487864f6e1f5f
pmutomb <53643665+pmutomb@users.noreply.github.com>
2020-10-13 20:18:40 -0400

    Step 3 - Add createPhoneIntent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,4 +41,9 @@ public void createMapIntent (View view){
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        Intent callNumber = new Intent (Intent.ACTION_DIAL);
+        callNumber.setData(Uri.parse("tel:0123456789"));
+        startActivity(callNumber);
+    }
 }
